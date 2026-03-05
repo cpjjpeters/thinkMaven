40f024b0603ccac0658741a647093f299b0c3384
Carl Peters <31207893+cpjjpeters@users.noreply.github.com>
2026-03-05 13:05:31 +0100

    you can debug and use f8

--- src/main/java/be/ipeters/thinkmaven/HomeController.java
+++ src/main/java/be/ipeters/thinkmaven/HomeController.java
@@ -1,5 +1,19 @@
 package be.ipeters.thinkmaven;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /* carlpeters created on 05/03/2026 inside the package - be.ipeters.thinkmaven */
+@Controller
 public class HomeController {
+    @RequestMapping("/")
+    public String index() {
+        String viewName = getViewName();
+        return viewName;
+//        return "index.html";
+    }
+
+    private String getViewName() {
+        return "index";
+    }
 }
--- src/main/java/be/ipeters/thinkmaven/ThinkMavenApplication.java
+++ src/main/java/be/ipeters/thinkmaven/ThinkMavenApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ThinkMavenApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ThinkMavenApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ThinkMavenApplication.class, args);
+    }
 
 }
